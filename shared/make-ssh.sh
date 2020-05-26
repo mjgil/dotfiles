7b8b24a6d3cfd182bc3b03a0942e2085445c8511
Malcom Gilbert <malcomgilbert@gmail.com>
2020-05-25 21:29:08 -0500

    Update make-ssh.sh

--- shared/make-ssh.sh
+++ shared/make-ssh.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 ssh-keygen -t rsa -b 4096 -C "malcomgilbert@gmail.com" -N "" -f "$HOME/.ssh/id_rsa"
-cat ~/.ssh/id_rsa.pub
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
+echo ""
 echo "printing ssh-key:"
 cat ~/.ssh/id_rsa.pub
