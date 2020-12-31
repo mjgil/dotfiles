fd4cb125e4d076f9256c909527efef6700fbf0cf
Malcom Gilbert <malcomgilbert@gmail.com>
2020-12-31 05:45:34 -0600

    add dropbox instructions

--- ubuntu/installUbuntuDesktop.sh
+++ ubuntu/installUbuntuDesktop.sh
@@ -102,3 +102,11 @@ gsettings set org.gnome.desktop.interface clock-format 12h
 
 # generate new ssh key for github
 ~/git/dotfiles/shared/make-ssh.sh
+
+
+# install dropbox
+# sudo nano /etc/apt/sources.list.d/dropbox.list
+# add line -- deb [arch=i386,amd64] http://linux.dropbox.com/ubuntu bionic main
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
+# sudo apt update
+# sudo apt install python3-gpg dropbox
