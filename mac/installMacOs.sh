6fc6cf2d5ce8d3e1174813999ac8e7fda87038ed
Malcom Gilbert <malcomgilbert@gmail.com>
2022-10-21 21:01:58 -0500

    change email

--- mac/installMacOs.sh
+++ mac/installMacOs.sh
@@ -4,7 +4,7 @@ sudo xcodebuild -license
 
 # Setup Git
 git config --global user.name "Malcom Gilbert"
-git config --global user.email malcom@mjgil.com
+git config --global user.email malcomgilbert@gmail.com
 git config --global core.editor "subl -n -w"
 git config --global push.default matching
 git config --global core.excludesfile ~/.gitignore
@@ -31,4 +31,4 @@ git clone https://github.com/mjgil/z.git
 /Users/$(whoami)/git/dotfiles/mac/installSettingsAndApps.sh
 /Users/$(whoami)/git/dotfiles/mac/update-bashrc.sh
 
-ssh-keygen -t rsa -b 4096 -C "malcom@mjgil.com"
+ssh-keygen -t rsa -b 4096 -C "malcomgilbert@gmail.com"
