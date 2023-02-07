1988b7f42e2ae9416d7e46900a121c11c175e922
Malcom Gilbert <malcomgilbert@gmail.com>
2023-02-07 14:40:18 -0600

    update dotfiles mac to work with zsh

--- mac/update-bashrc.sh
+++ mac/update-bashrc.sh
@@ -2,5 +2,7 @@
 
 rm ~/.bash_profile
 rm ~/.bashrc
+rm ~/.zshrc
 ln -s /Users/$(whoami)/git/dotfiles/mac/.bash_profile ~/.bash_profile
 ln -s /Users/$(whoami)/git/dotfiles/shared/.bashrc ~/.bashrc
+ln -s /Users/$(whoami)/git/dotfiles/mac/.zshrc ~/.zshrc
