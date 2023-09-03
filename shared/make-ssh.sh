ebb0e3d2bc33129752fb396b9192a73a38681bfa
Malcom Gilbert <malcomgilbert@gmail.com>
2023-09-02 20:43:11 -0500

    rm make ssh

--- shared/make-ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ssh-keygen -t rsa -b 4096 -C "malcomgilbert@gmail.com" -N "" -f "$HOME/.ssh/id_rsa"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-
-echo ""
-echo "printing ssh-key:"
-cat ~/.ssh/id_rsa.pub
