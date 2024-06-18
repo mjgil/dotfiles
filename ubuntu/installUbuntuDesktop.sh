74afdb98c5c9ac79c5966c0946419c7172bf82cd
Malcom Gilbert <malcomgilbert@gmail.com>
2024-06-18 05:00:06 -0500

    fix custom keyboard shortcut

--- ubuntu/installUbuntuDesktop.sh
+++ ubuntu/installUbuntuDesktop.sh
@@ -159,20 +159,32 @@ gsettings set org.cinnamon.desktop.interface clock-use-24h false
 gsettings set org.cinnamon.desktop.interface clock-show-date true
 gsettings set org.nemo.preferences default-folder-viewer 'list-view'
 
-gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ name 'Area Screenshot'
-gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ binding "['<Primary>Print']"
-gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ command "gnome-screenshot -a"
+
+
+
+
 
 current_list=$(gsettings get org.cinnamon.desktop.keybindings custom-list)
-# Check if the list is empty or already contains 'custom0'
-if [[ "$current_list" == "@as []" ]]; then
-  # If the list is empty, create a new list with 'custom0'
-  new_list="@as ['custom0']"
+# Check if the list already contains 'custom0'
+if echo "$current_list" | grep -q "'custom0'"; then
+  echo "'custom0' is already in the list"
 else
-  # If the list is not empty, append 'custom0' to the list
-  new_list=$(echo "$current_list" | sed "s/]/, 'custom0']/g")
+  gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ name 'Area Screenshot'
+  gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ binding "['<Primary>Print']"
+  gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ command "gnome-screenshot -a"
+  # Check if the list is empty
+  if [[ "$current_list" == "@as []" ]]; then
+    # If the list is empty, create a new list with 'custom0'
+    new_list="@as ['custom0']"
+  else
+    # If the list is not empty, append 'custom0' to the list
+    new_list=$(echo "$current_list" | sed "s/]/, 'custom0']/g")
+  fi
+
+  # Set the updated list of custom keybindings
+  gsettings set org.cinnamon.desktop.keybindings custom-list "$new_list"
 fi
-gsettings set org.cinnamon.desktop.keybindings custom-list "$new_list"
+
 
 # don't group applications by window
 for file in ~/.cinnamon/configs/grouped-window-list@cinnamon.org/*.json; do
