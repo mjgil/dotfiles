bfc42b93aacb376573341daf57f57522c1a3a3b8
Malcom Gilbert <malcomgilbert@gmail.com>
2024-09-01 17:51:18 -0500

    add sqlite3

--- check-installed.sh
+++ check-installed.sh
@@ -6,7 +6,8 @@ programs=("git" "hub" "curl" "jq" "google-chrome" "vlc" "tree" "ffmpeg"
           "python" "pip" "python3" "python3.9" "pip3" "pipenv" "yt-dlp"
           "brave" "gimp" "go" "cargo" "n" "dfg" "dua" "dug" "dum" "fa"
           "ms" "zipr" "subl" "make" "cmake" "gcloud" "snap" "g++" 
-          "g++-10" "mvn" "fastfetch" "asdf" "rg" "ncdu" "dotnet" "cal")
+          "g++-10" "mvn" "fastfetch" "asdf" "rg" "ncdu" "dotnet" "cal"
+          "sqlite3")
 
 # Counters for passed and failed checks
 passed=0
